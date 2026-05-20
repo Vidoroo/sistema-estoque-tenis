f72b7ef5bf462300df5f89e4a26f7e859db8e46c
Vidoroo <victorpvp1000@gmail.com>
2026-05-20 16:59:44 -0300

    Update Produtos.tsx

--- src/pages/Produtos.tsx
+++ src/pages/Produtos.tsx
@@ -8,7 +8,8 @@ type Product = {
   quantity: number;
   price: number;
   preco_varejo: number;
-  preco_atacado: number;`n  preco_dropshipping: number;
+  preco_atacado: number;
+  preco_dropshipping: number;
   image: string;
   tamanhos: Record<string, string>;
   fornecedor: string | null;
